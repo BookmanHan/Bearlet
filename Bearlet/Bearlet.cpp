936f095e15eacd7f6122d3a13da548fb7510f011
Xiaohan Bookman <Bookman@vip.163.com>
2017-10-01 23:37:03 +0800

    Revise for constant.

--- Bearlet/Bearlet.cpp
+++ Bearlet/Bearlet.cpp
@@ -23,30 +23,33 @@ Symbol& sigmoid_layer(Symbol& x, int n_input, int n_output)
 {
 	iGraph& model = *x.model;
 	autoref W = model.variable("Hidden Layer", xavier_initial(n_input, n_output));
-	return sigm(x * W);
+	return sigm(abs(x) * W);
 }
 
 int main(int, char* argv[])
 {
-	dmMNIST ldMNIST;
-	ldMNIST.load();
-	int size_feature = ldMNIST.arr_train_data.dims(1);
+	dmCIFAR10 loader;
+	loader.load();
+
+	int size_feature = loader.arr_train_data.dims(1);
 
 	iGraph model;
 		
-	autoref label = model.data_source("label", label_vectorization(ldMNIST.arr_train_label, 10));
-	autoref data = model.data_source("data", ldMNIST.arr_train_data/255.f);
+	autoref label = model.data_source("label", label_vectorization(loader.arr_train_label, 10));
+	autoref data = model.data_source("data", loader.arr_train_data/255.f);
 
 	autoref hid_1 = sigmoid_layer(data, size_feature, 300);
-	autoref hid_2 = sigmoid_layer(hid_1, 300, 300);
-	autoref y = sigmoid_layer(hid_2, 300, 10);
+	autoref t = sigmoid_layer(hid_1, 300, 500);
+	autoref y = sigmoid_layer(t, 500, 10);
 
 	model.loss("Prediction", (y - label) % (y - label)); 
+	model.loss("Diversity", abs(t));
+	model.loss("Diversity", abs(hid_1));
 
 	model.train(atoi(argv[1]));
 		
-	label.set(label_vectorization(ldMNIST.arr_test_label, 10));
-	data.set(ldMNIST.arr_test_data/255.f);
+	label.set(label_vectorization(loader.arr_test_label, 10));
+	data.set(loader.arr_test_data/255.f);
 	
 	model.perform();
 	model.report();
@@ -55,8 +58,8 @@ int main(int, char* argv[])
 	af::array val_pred;
 	af::max(val_pred, idx_pred, y.value_forward, 1);
 	
-	af::array acc = sum(idx_pred == ldMNIST.arr_test_label);
-	logout.record() << ::print_array(acc/(float)ldMNIST.arr_test_label.dims(0));
+	af::array acc = sum(idx_pred == loader.arr_test_label);
+	logout.record() << ::print_array(acc/(float)loader.arr_test_label.dims(0));
 
 
 	return 0;
